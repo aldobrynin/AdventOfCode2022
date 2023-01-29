1eca3bacde58d36ab51697d8234fc8973974aedb
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-01-29 15:45:10 +0400

    day 5

---
+++ AoC2020/Day5.cs
@@ -0,0 +1,26 @@
+using Common;
+
+namespace AoC2020;
+
+public class Day5
+{
+    public static void Solve(IEnumerable<string> input)
+    {
+        var seats = input
+            .Select(Decode)
+            .OrderBy(x => x)
+            .ToArray();
+
+        seats[^1].Dump("Part1: ");
+
+        (seats
+                .Where((seatId, ind) => ind < seats.Length - 1 && seatId + 1 != seats[ind + 1])
+                .First() + 1)
+            .Dump("Part2: ");
+    }
+
+    private static int Decode(string code)
+    {
+        return code.Aggregate(0, (cur, ch) => cur * 2 + (ch is 'B' or 'R' ? 1 : 0));
+    }
+}
