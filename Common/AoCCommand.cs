757bf732e71113faae8255b110a8f130d5aab15c
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-11-16 14:03:55 +0400

    share input type via global static context, print current year

--- Common/AoCCommand.cs
+++ Common/AoCCommand.cs
@@ -35,9 +35,10 @@ public class AoCCommand : Command<AoCCommand.AoCCommandSettings>
         return int.TryParse(typeName.Replace("Day", string.Empty), out var day) ? day : -1;
     }
 
-    private static void RunSolution((TypeInfo Type, int Day) dayType, bool sample)
-    {
-        AnsiConsole.MarkupLine(":christmas_tree::christmas_tree::christmas_tree:Day [underline red]{0}[/] of AoC 2022:christmas_tree::christmas_tree::christmas_tree:", dayType.Day);
+    private static void RunSolution((TypeInfo Type, int Day) dayType, bool sample, int year) {
+        AnsiConsole.MarkupLine(
+            ":christmas_tree::christmas_tree::christmas_tree:Day [underline red]{0}[/] of AoC {1}:christmas_tree::christmas_tree::christmas_tree:",
+            dayType.Day, year);
         var method = dayType.Type.GetMethod("Solve", BindingFlags.Public | BindingFlags.Static)
                      ?? throw new Exception($"Solve method is not found in {dayType.Type.Name}");
         var dayInputDirectory = Path.Combine("inputs", $"day{dayType.Day}");
@@ -45,6 +46,7 @@ public class AoCCommand : Command<AoCCommand.AoCCommandSettings>
         AnsiConsole.MarkupLine("Using input from [underline navy]{0}[/]", inputFile);
         var input = File.ReadLines(inputFile);
         var action = (Action<IEnumerable<string>>)Delegate.CreateDelegate(typeof(Action<IEnumerable<string>>), method);
+        AoCContext.IsSample = sample;
         action(input);
     }
 
@@ -52,7 +54,9 @@ public class AoCCommand : Command<AoCCommand.AoCCommandSettings>
     public override int Execute(CommandContext context, AoCCommandSettings settings)
 #pragma warning restore CS8765
     {
-        var dayClass = Assembly.GetEntryAssembly()
+        var entryAssembly = Assembly.GetEntryAssembly();
+        var year = int.Parse(entryAssembly!.GetName().Name!.Replace("AoC", string.Empty, StringComparison.OrdinalIgnoreCase));
+        var dayClass = entryAssembly
             .DefinedTypes.Where(x => x.Name.StartsWith("Day"))
             .Select(x => (Type: x, Day: GetDayNumber(x.Name)))
             .Where(x => settings.Day == null || x.Day == settings.Day.Value);
@@ -60,9 +64,8 @@ public class AoCCommand : Command<AoCCommand.AoCCommandSettings>
             ? dayClass.OrderBy(x => x.Day)
             : dayClass.OrderByDescending(x => x.Day).Take(1);
 
-        foreach (var tuple in dayClass)
-        {
-            Measure.Time(() => RunSolution(tuple, settings.Sample))
+        foreach (var tuple in dayClass) {
+            Measure.Time(() => RunSolution(tuple, settings.Sample, year))
                 .Dump("Finished in ");
         }
 
---
+++ Common/AoCContext.cs
@@ -0,0 +1,5 @@
+namespace Common;
+
+public static class AoCContext {
+    public static bool IsSample { get; set; }
+}
