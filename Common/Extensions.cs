6d77011863d04d2ca44478f736eebca0267b1d95
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-11-25 17:19:05 +0400

    day 20

--- Common/Extensions.cs
+++ Common/Extensions.cs
@@ -1,50 +1,46 @@
 using System.Collections;
-using System.Text;
 
 namespace Common;
 
-public static class Extensions
-{
+public static class Extensions {
     public static V Find<T>(this T[][] map, T value) => map.FindAll(value).First();
 
-    public static T Get<T>(this T[][] map, V pos)
-    {
+    public static T Get<T>(this T[][] map, V pos) {
         if (pos.IsInRange(map))
             return map[pos.Y][pos.X];
         throw new ArgumentOutOfRangeException(nameof(pos), pos, "Index out of range");
     }
 
-    public static T Get<T>(this T[,] map, V pos)
-    {
+    public static T Get<T>(this T[,] map, V pos) {
         if (pos.IsInRange(map))
             return map[pos.Y, pos.X];
         throw new ArgumentOutOfRangeException(nameof(pos), pos, "Index out of range");
     }
 
-    public static void Set<T>(this T[][] map, V pos, T value)
-    {
+    public static void Set<T>(this T[][] map, V pos, T value) {
         if (pos.IsInRange(map))
             map[pos.Y][pos.X] = value;
         else
             throw new ArgumentOutOfRangeException(nameof(pos), pos, "Index out of range");
     }
 
-    public static void Set<T>(this T[,] map, V pos, T value)
-    {
+    public static void Set<T>(this T[,] map, V pos, T value) {
         if (pos.IsInRange(map))
             map[pos.Y, pos.X] = value;
         else
             throw new ArgumentOutOfRangeException(nameof(pos), pos, "Index out of range");
     }
 
-    public static IEnumerable<V> FindAll<T>(this T[][] map, T value)
-    {
+    public static IEnumerable<V> FindAll<T>(this T[][] map, T value) {
         for (var y = 0; y < map.Length; y++)
-        for (var x = 0; x < map[y].Length; x++)
-        {
+        for (var x = 0; x < map[y].Length; x++) {
             if (Equals(map[y][x], value))
                 yield return new(x, y);
         }
+    }
+    
+    public static IEnumerable<V> FindAll<T>(this Map<T> map, T value) {
+        return map.Coordinates().Where(v => Equals(map[v], value));
     }
 
     public static int Product(this IEnumerable<int> source) => source.Aggregate(1, (x, y) => x * y);
@@ -57,17 +53,21 @@ public static class Extensions
 
     public static long Product(this IEnumerable<long> source) => source.Aggregate(1L, (x, y) => x * y);
 
-    public static T Dump<T>(this T source, string? message = null, Func<T, object>? transform = null)
-    {
+    public static T Dump<T>(this T source, string? message = null, Func<T, object>? transform = null,
+        string? separator = ",") {
         var printObject = transform == null ? source : transform(source);
-        var sb = new StringBuilder();
-        if (message != null)
-            sb.Append(message);
-        if (printObject is IEnumerable enumerable and not string)
-            sb.AppendJoin(",", Stringify(enumerable));
-        else
-            sb.Append(printObject);
-        Console.WriteLine(sb.ToString());
+        if (message != null) Console.Out.Write(message);
+        if (printObject is IEnumerable enumerable and not string) {
+            var isFirst = true;
+            foreach (var s in Stringify(enumerable)) {
+                if (!isFirst) Console.Out.Write(separator);
+                Console.Out.Write(s);
+                isFirst = false;
+            }
+        }
+        else Console.Out.Write(printObject);
+
+        Console.Out.WriteLine();
         return source;
     }
 
@@ -75,19 +75,15 @@ public static class Extensions
     public static long SetBit(this long v, int bitIndex) => v | (1L << bitIndex);
 
 
-    public static IEnumerable<int> Indices<T>(this IEnumerable<T> source)
-    {
+    public static IEnumerable<int> Indices<T>(this IEnumerable<T> source) {
         return source.Select((_, i) => i);
     }
 
-    public static T[][] Transpose<T>(this T[][] source)
-    {
+    public static T[][] Transpose<T>(this T[][] source) {
         var copy = new T[source[0].Length][];
-        for (int x = 0; x < source[0].Length; x++)
-        {
+        for (int x = 0; x < source[0].Length; x++) {
             copy[x] = new T[source.Length];
-            for (int y = 0; y < source.Length; y++)
-            {
+            for (int y = 0; y < source.Length; y++) {
                 copy[x][y] = source[y][x];
             }
         }
@@ -95,22 +91,18 @@ public static class Extensions
         return copy;
     }
 
-    public static IEnumerable<V> Coordinates<T>(this T[][] map)
-    {
+    public static IEnumerable<V> Coordinates<T>(this T[][] map) {
         for (var y = 0; y < map.Length; y++)
         for (var x = 0; x < map[y].Length; x++)
             yield return new V(x, y);
 
     }
 
-    public static T[,] DumpMap<T>(this T[,] source, string? message = null, Func<T, object>? transform = null)
-    {
+    public static T[,] DumpMap<T>(this T[,] source, string? message = null, Func<T, object>? transform = null) {
         if (message != null)
             Console.WriteLine(message);
-        for (int i = 0; i < source.GetLength(0); i++)
-        {
-            for (int j = 0; j < source.GetLength(1); j++)
-            {
+        for (int i = 0; i < source.GetLength(0); i++) {
+            for (int j = 0; j < source.GetLength(1); j++) {
                 var print = transform == null ? source[i, j] : transform(source[i, j]);
                 Console.Write(print);
             }
@@ -121,30 +113,41 @@ public static class Extensions
         return source;
     }
 
-    private static IEnumerable<string?> Stringify(IEnumerable enumerable)
-    {
-        foreach (var item in enumerable)
-        {
+    private static IEnumerable<string?> Stringify(IEnumerable enumerable) {
+        foreach (var item in enumerable) {
             yield return item?.ToString();
         }
     }
 
-    public static string StringJoin<T>(this IEnumerable<T> source, string? separator = ",")
-    {
+    public static string StringJoin<T>(this IEnumerable<T> source, string? separator = ",") {
         return string.Join(separator, source);
     }
 
     public static int Mod(this int value, int divisor) => (value % divisor + divisor) % divisor;
 
 
-    public static IEnumerable<T[]> Rows<T>(this T[][] source)
-    {
+    public static IEnumerable<T[]> Rows<T>(this T[][] source) {
         return source.Select((_, y) => source.ElementAt(y));
     }
 
-    public static IEnumerable<T[]> Columns<T>(this T[][] source)
-    {
+    public static IEnumerable<T[]> Columns<T>(this T[][] source) {
         for (var x = 0; x < source[0].Length; x++)
             yield return source.Rows().Select(r => r[x]).ToArray();
+    }
+
+    public static IEnumerable<IReadOnlyList<string>> SplitBy(
+        this IEnumerable<string> source,
+        Func<string, bool> isSeparator) {
+        var list = new List<string>();
+        foreach (var s in source) {
+            if (isSeparator(s)) {
+                yield return list;
+                list = new List<string>();
+            }
+            else list.Add(s);
+        }
+
+        if (list.Count > 0)
+            yield return list;
     }
 }
--- Common/Map.cs
+++ Common/Map.cs
@@ -36,6 +36,12 @@ public class Map<T>
         set => _arr.Set(key, value);
     }
 
+    public T this[int row, int col]
+    {
+        get => _arr[row][col];
+        set => _arr[row][col] = value;
+    }
+
     public IEnumerable<V> Area4(V v)
     {
         return v.Area4().Where(x => x.IsInRange(_arr));
@@ -45,6 +51,45 @@ public class Map<T>
     {
         return v.Area8().Where(x => x.IsInRange(_arr));
     }
+
+    public IEnumerable<T[]> Rows() => _arr.Rows();
+
+    public IEnumerable<T[]> Columns() => _arr.Columns();
+
+    public IEnumerable<T[]> Borders() {
+        var rows = Rows().Where((_, i) => i == 0 || i == SizeY - 1).ToArray();
+        var columns = Columns().Where((_, i) => i == 0 || i == SizeX - 1).ToArray();
+        return new[] { columns[0], rows[0], columns[1], rows[1] };
+    }
+
+    public Map<T> Rotate90() {
+        var newMap = new Map<T>(SizeY, SizeX);
+        foreach (var coordinate in Coordinates()) {
+            var newCoordinate = new V(SizeY - 1 - coordinate.Y, coordinate.X);
+            newMap[newCoordinate] = this[coordinate];
+        }
+
+        return newMap;
+    }
+    
+    public Map<T> Rotate180() {
+        return Rotate90().Rotate90();
+    }
+    
+    public Map<T> Rotate270() {
+        return Rotate180().Rotate90();
+    }
+    
+    public Map<T> Flip() {
+        var newMap = new Map<T>(SizeY, SizeX);
+        foreach (var coordinate in Coordinates()) {
+            var newCoordinate = new V(coordinate.Y, coordinate.X);
+            newMap[newCoordinate] = this[coordinate];
+        }
+
+        return newMap;
+    }
+    
 
     public void Print()
     {
@@ -58,7 +103,6 @@ public class Map<T>
     public Map<T> Clone() => new(_arr);
 }
 
-public class Map
-{
+public class Map {
     public static Map<T> From<T>(T[][] source) => new(source);
 }
