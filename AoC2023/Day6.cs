ee7924f1872f2abbe22a073ebbe1b0ef4056bac6
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-12-06 10:54:20 +0400

    day 6

---
+++ AoC2023/Day6.cs
@@ -0,0 +1,27 @@
+namespace AoC2023;
+
+public class Day6 {
+    public static void Solve(IEnumerable<string> input) {
+        var values = input.Select(line => line.Split(':')[1]).ToArray();
+        var times = values[0];
+        var distances = values[1];
+        times.ToLongArray()
+            .Zip(distances.ToLongArray(), Solve)
+            .Product()
+            .Dump("Part1: ");
+
+        var totalTime = times.Replace(" ", string.Empty).ToLong();
+        var totalDistance = distances.Replace(" ", string.Empty).ToLong();
+        Solve(totalTime, totalDistance)
+            .Dump("Part2: ");
+    }
+
+    // raceTime*x - x^2 > distance
+    // x^2 - raceTime*x + distance < 0
+    private static long Solve(long raceTime, long distance) {
+        var discriminant = Math.Sqrt(raceTime * raceTime - 4 * distance);
+        var x1 = (raceTime + discriminant) / 2;
+        var x2 = (raceTime - discriminant) / 2;
+        return (long)(Math.Ceiling(x1) - 1 - Math.Floor(x2));
+    }
+}
