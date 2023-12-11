b0915875ce898230cc74c163904b50f548c67f51
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-12-11 13:00:41 +0400

    fix day 10: find really matching tile for S

--- AoC2023/Day10/Day10.cs
+++ AoC2023/Day10/Day10.cs
@@ -3,31 +3,21 @@ using Range = Common.Range;
 namespace AoC2023.Day10;
 
 public class Day10 {
-    private static readonly Dictionary<char, V[]> Tiles =
-        new() {
-            ['|'] = new[] { V.Up, V.Down, },
-            ['-'] = new[] { V.Left, V.Right, },
-            ['L'] = new[] { V.Right, V.Down, },
-            ['J'] = new[] { V.Left, V.Down, },
-            ['F'] = new[] { V.Up, V.Right, },
-            ['7'] = new[] { V.Left, V.Up, },
-            ['.'] = Array.Empty<V>(),
-        };
-
     public static void Solve(IEnumerable<string> input) {
         var map = Map.From(input);
         var start = map.FindAll('S').Single();
-        var (startTile, _) = Tiles
-            .Single(kv => kv.Value.Count(dir => AreTilesConnected(kv.Key, map[start + dir], dir)) == 2)
-            .Dump("Possible start tiles: ");
-        map[start] = startTile;
+        var longestLoop = map.Dfs((from, to) => AreTilesConnected(map[from], map[to], to - from), start)
+            .Where(x => x.Distance > 1 && start.Area4().Contains(x.State))
+            .MaxBy(x => x.Distance) ?? throw new Exception("No loop found");
+        ((longestLoop.Distance + 1) / 2).Dump("Part1: ");
+        var pipe = longestLoop.FromStart().ToArray();
 
-        var pipe = map
-            .Bfs((from, to) => AreTilesConnected(map[from], map[to], to - from), start)
-            .Select(x => x.State)
-            .ToHashSet();
-        (pipe.Count / 2).Dump("Part1: ");
-        FindIsolatedCells(map, pipe).Dump("Part2: ");
+        var adjacentToStart = new[] { pipe[1], longestLoop.State };
+        var startTile = Tiles.Keys
+            .Where(x => x != 'S')
+            .Single(tile => adjacentToStart.All(n => AreTilesConnected(map[n], tile, start - n)));
+        map[start] = startTile;
+        FindIsolatedCells(map, pipe.ToHashSet()).Dump("Part2: ");
     }
 
     private static int FindIsolatedCells(Map<char> map, HashSet<V> pipe) {
@@ -72,23 +62,36 @@ public class Day10 {
     private static bool AreTilesConnected(char from, char to, V dir) =>
         Tiles[from].Contains(dir) && Tiles[to].Contains(-dir);
 
+    private static readonly Dictionary<char, V[]> Tiles =
+        new() {
+            ['|'] = new[] { V.Up, V.Down, },
+            ['-'] = new[] { V.Left, V.Right, },
+            ['L'] = new[] { V.Right, V.Down, },
+            ['J'] = new[] { V.Left, V.Down, },
+            ['F'] = new[] { V.Up, V.Right, },
+            ['7'] = new[] { V.Left, V.Up, },
+            ['S'] = V.Directions4,
+            ['.'] = Array.Empty<V>(),
+        };
+
+    private static readonly Dictionary<char, string> PrintMap = new() {
+        ['|'] = "│",
+        ['-'] = "─",
+        ['F'] = "┌",
+        ['7'] = "┐",
+        ['L'] = "└",
+        ['J'] = "┘",
+        ['S'] = "S",
+        ['.'] = ".",
+    };
+
     private static void PrintColored(Map<char> map, IReadOnlySet<V> pipe, IReadOnlySet<V>? outerCells = null) {
         var isolatedCells = map.Coordinates().Except(pipe).Except(outerCells ?? new HashSet<V>()).ToHashSet();
-        var printMap = new Dictionary<char, string> {
-            ['|'] = "│",
-            ['-'] = "─",
-            ['F'] = "┌",
-            ['7'] = "┐",
-            ['L'] = "└",
-            ['J'] = "┘",
-        };
-        map.PrintColored(Printer);
+        map.PrintColored(FormatCell);
 
-        return;
-
-        (string, ConsoleColor?) Printer(V v) =>
+        (string, ConsoleColor?) FormatCell(V v) =>
             (pipe.Contains(v), outerCells?.Contains(v), isolatedCells.Contains(v)) switch {
-                (true, _, _) => (printMap[map[v]], ConsoleColor.Green),
+                (true, _, _) => (PrintMap[map[v]], ConsoleColor.Green),
                 (_, true, _) => ("O", ConsoleColor.Yellow),
                 (_, _, true) => ("I", ConsoleColor.Red),
                 _ => (map[v].ToString(), null)
