e4aa252fa3d3a6dd7b089557819ce30f0f353ca5
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-12-12 19:40:00 +0400

    day 12: dp solution

--- AoC2023/Day12/Day12.cs
+++ AoC2023/Day12/Day12.cs
@@ -18,33 +18,67 @@ public partial class Day12 {
             .Select(x => new ConditionRecord(x[0], x[1].ToIntArray()))
             .ToArray();
 
-        maps.Sum(Count).Part1();
-        maps.Sum(x => Count(x.Unfold(5))).Part2();
+        maps.AsParallel().Sum(x => CountArrangements(x)).Part1();
+        maps.AsParallel().Sum(x => CountArrangements(x.Unfold(5))).Part2();
     }
 
-    private static long Count(ConditionRecord input) => CountMemoized(input, cache: new());
+    private static long CountArrangements(ConditionRecord input, bool useMemo = false) =>
+        useMemo ? CountMemoized(input) : CountDp(input);
 
-    private static long CountMemoized(ConditionRecord input,
-        int springIndex = 0, int groupIndex = 0, int currentGroup = 0, Dictionary<CacheItem, long> cache = null!) {
-
-        var cacheKey = (springIndex, groupIndex, currentGroup);
-        if (cache.TryGetValue(cacheKey, out var cached)) return cached;
+    private static long CountDp(ConditionRecord input) {
         var (springs, groups) = input;
+        var dp = new long[springs.Length + 1];
+        var next = new long[springs.Length + 1];
+        dp[0] = 1;
+        for (var i = 0; i < springs.Length && springs[i] != '#'; i++)
+            dp[i + 1] = 1;
 
-        if (springIndex == springs.Length)
-            return groups.Length == groupIndex && currentGroup == 0 ||
-                   groups.Length - 1 == groupIndex && currentGroup == groups[groupIndex]
-                ? 1
-                : 0;
+        for (var index = 0; index < groups.Length; index++) {
+            var groupSize = groups[index];
+            next[0] = 0;
+            var currentGroup = 0;
 
-        var count = 0L;
-        if (springs[springIndex] is '#' or '?')
-            count += CountMemoized(input, springIndex + 1, groupIndex, currentGroup + 1, cache);
+            for (var springIndex = 0; springIndex < springs.Length; springIndex++) {
+                next[springIndex + 1] = springs[springIndex] is '.' or '?' ? next[springIndex] : 0;
+                currentGroup = springs[springIndex] is '#' or '?' ? currentGroup + 1 : 0;
 
-        var isEmptyOrCompleted = currentGroup == 0 || groupIndex < groups.Length && currentGroup == groups[groupIndex];
-        if (springs[springIndex] is '.' or '?' && isEmptyOrCompleted)
-            count += CountMemoized(input, springIndex + 1, groupIndex + Math.Sign(currentGroup), 0, cache);
+                if (currentGroup < groupSize) continue;
+                var groupStartIndex = springIndex - groupSize + 1;
+                if (groupStartIndex == 0 || springs[groupStartIndex - 1] != '#')
+                    next[springIndex + 1] += groupStartIndex == 0 ? dp[0] : dp[groupStartIndex - 1];
+            }
 
-        return cache[cacheKey] = count;
+            (next, dp) = (dp, next);
+        }
+
+        return dp[^1];
+    }
+
+    private static long CountMemoized(ConditionRecord input) {
+        var (springs, groups) = input;
+        var cache = new Dictionary<CacheItem, long>();
+        return Count(springIndex: 0, groupIndex: 0, currentGroup: 0);
+
+        long Count(int springIndex, int groupIndex, int currentGroup) {
+            var cacheKey = (springIndex, groupIndex, currentGroup);
+            if (cache.TryGetValue(cacheKey, out var cached)) return cached;
+
+            if (springIndex == springs.Length)
+                return groups.Length == groupIndex && currentGroup == 0 ||
+                       groups.Length - 1 == groupIndex && currentGroup == groups[groupIndex]
+                    ? 1
+                    : 0;
+
+            var count = 0L;
+            if (springs[springIndex] is '#' or '?')
+                count += Count(springIndex + 1, groupIndex, currentGroup + 1);
+
+            var isEmptyOrCompleted =
+                currentGroup == 0 || groupIndex < groups.Length && currentGroup == groups[groupIndex];
+            if (springs[springIndex] is '.' or '?' && isEmptyOrCompleted)
+                count += Count(springIndex + 1, groupIndex + Math.Sign(currentGroup), 0);
+
+            return cache[cacheKey] = count;
+        }
     }
 }
