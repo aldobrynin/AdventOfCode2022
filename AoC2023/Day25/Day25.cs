dfd2e1956367ad933b9375a4f3683850a5faf5a6
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-12-25 17:42:15 +0400

    fast mincut

--- AoC2023/Day25/Day25.cs
+++ AoC2023/Day25/Day25.cs
@@ -1,13 +1,10 @@
+using System.Collections.Frozen;
+
 namespace AoC2023.Day25;
 
 public static partial class Day25 {
     public record Graph(List<string> Vertices, List<(string From, string To)> Edges) {
-        public Dictionary<string, List<string>> ToAdjacencyMap() {
-            return Edges.SelectMany(x => new[] { x, (From: x.To, To: x.From) })
-                .Where(x => x.From != x.To)
-                .GroupBy(x => x.From, x => x.To)
-                .ToDictionary(x => x.Key, x => x.ToList());
-        }
+        public IReadOnlyDictionary<string, int> VertPowers { get; init; } = Vertices.ToDictionary(x => x, _ => 1);
     }
 
     public static void Solve(IEnumerable<string> input) {
@@ -19,26 +16,29 @@ public static partial class Day25 {
         var graph = new Graph(vertices, edges);
 
         foreach (var _ in 1.RangeTo(1000)) {
-            var contractedGraph = ContractGraph(graph);
-            var removed = graph.Edges.Except(contractedGraph.Edges).ToList();
-            if (removed.Count != 3) continue;
-
-            var adj = contractedGraph.ToAdjacencyMap();
-            new[] { removed[0].From, removed[0].To }
-                .Product(node => SearchHelpers.Bfs(cur => adj[cur], initialStates: [node]).Count())
-                .Part1();
+            var contracted = FastMinCut(graph);
+            if (contracted.Edges.Count != 3) continue;
+            contracted.VertPowers.Product(x => x.Value).Part1();
             return;
         }
 
         throw new Exception("No solution found");
     }
 
-    private static Graph ContractGraph(Graph graph) {
+    private static Graph FastMinCut(Graph graph) {
+        if (graph.Vertices.Count <= 6) return MinCut(graph, 2);
+        var t = (int)(1 + graph.Vertices.Count / Math.Sqrt(2));
+        return Enumerable.Range(0, 2)
+            .Select(_ => FastMinCut(MinCut(graph, t)))
+            .MinBy(x => x.Edges.Count)!;
+    }
+
+    private static Graph MinCut(Graph graph, int t) {
         var (vertices, edges) = graph;
         var components = graph.Vertices.ToDictionary(x => x, x => new List<string> { x });
         var vertexToComponent = vertices.ToDictionary(x => x);
 
-        while (components.Count > 2) {
+        while (components.Count > t) {
             var (from, to) = edges[Random.Shared.Next(edges.Count)];
 
             var (fromComponent, toComponent) = (vertexToComponent[from], vertexToComponent[to]);
@@ -50,6 +50,11 @@ public static partial class Day25 {
             }
         }
 
-        return graph with { Edges = edges.Where(e => vertexToComponent[e.From] == vertexToComponent[e.To]).ToList() };
+        var newVertices = components.Keys.ToList();
+        var newEdges = edges.Select(e => (vertexToComponent[e.From], vertexToComponent[e.To]))
+            .Where(x => x.Item1 != x.Item2)
+            .ToList();
+        var newPowers = components.ToFrozenDictionary(x => x.Key, x => x.Value.Sum(v => graph.VertPowers[v]));
+        return new Graph(newVertices, newEdges) { VertPowers = newPowers };
     }
 }
