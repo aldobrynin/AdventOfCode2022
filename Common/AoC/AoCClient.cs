36902177e88b2ff35598d22e3f2be1fd89e21b5b
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2024-12-05 22:41:47 +0400

    read session from file

--- Common/AoC/AoCClient.cs
+++ Common/AoC/AoCClient.cs
@@ -26,11 +26,28 @@ public static class AoCClient {
         return client;
     }
 
-    private static string GetAoCSession() {
-        var sessionFromEnv = Environment.GetEnvironmentVariable("AOC_SESSION");
-        if (!string.IsNullOrEmpty(sessionFromEnv)) return sessionFromEnv;
+    private static string GetAoCSession() => ReadSessionFromEnv() ?? ReadSessionFromFile() ?? PromptForSession();
+
+    private static string PromptForSession() {
         AnsiConsole.WriteLine("No session cookie was provided in AOC_SESSION environment variable");
         return AnsiConsole.Prompt(new TextPrompt<string>("Enter session cookie:").Secret())
             .Replace("session=", string.Empty);
+    }
+
+    private static string? ReadSessionFromEnv() {
+        var session = Environment.GetEnvironmentVariable("AOC_SESSION");
+        if (string.IsNullOrEmpty(session)) return null;
+
+        AnsiConsole.WriteLine("Reading session cookie from AOC_SESSION environment variable");
+        return session;
+
+    }
+
+    private static string? ReadSessionFromFile() {
+        var sessionFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".aoc_session");
+        if (!File.Exists(sessionFile)) return null;
+
+        AnsiConsole.WriteLine("Reading session cookie from {0}", sessionFile);
+        return File.ReadAllText(sessionFile);
     }
 }
