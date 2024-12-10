ea575d8de7703c0ca6f48f2362a1b941edb3b663
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2024-12-10 11:52:38 +0400

    refactor day10

--- AoC2024/Day10/Day10.cs
+++ AoC2024/Day10/Day10.cs
@@ -3,27 +3,23 @@ namespace AoC2024.Day10;
 public static partial class Day10 {
     public static void Solve(IEnumerable<string> input) {
         var map = Map.From(input);
-        var heads = map.FindAll('0').ToArray();
 
-        heads
-            .Sum(head => map.Bfs(CanMove, head).Count(x => map[x.State] == '9'))
-            .Part1();
+        var trailEnds = map
+            .FindAll('0')
+            .SelectMany(head => VisitAll(head).Where(v => map[v] == '9').CountBy(x => x))
+            .ToArray();
 
-        heads.Sum(CountTrails).Part2();
+        trailEnds.Length.Part1();
+        trailEnds.Sum(x => x.Value).Part2();
 
-        int CountTrails(V head) {
-            var queue = new Queue<V>([head]);
-            var trailsToNine = 0;
+        IEnumerable<V> VisitAll(V start) {
+            var queue = new Queue<V>([start]);
             while (queue.TryDequeue(out var current)) {
-                if (map[current] == '9') trailsToNine++;
-                foreach (var next in map.Area4(current).Where(next => CanMove(current, next))) {
+                yield return current;
+                foreach (var next in map.Area4(current).Where(next => map[next] - map[current] == 1)) {
                     queue.Enqueue(next);
                 }
             }
-
-            return trailsToNine;
         }
-
-        bool CanMove(V from, V to) => map[to] - map[from] == 1;
     }
 }
