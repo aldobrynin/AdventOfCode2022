814a17d835de98f685886655edc36f325a7455b1
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2024-12-13 21:49:56 +0400

    refactor

--- AoC2024/Day13/Day13.cs
+++ AoC2024/Day13/Day13.cs
@@ -5,17 +5,11 @@ namespace AoC2024.Day13;
 public static partial class Day13 {
     public record Machine(V ButtonA, V ButtonB, V Prize) {
         public static Machine Parse(IReadOnlyList<string> lines) {
-            return new Machine(ParseButton(lines[0]), ParseButton(lines[1]), ParsePrize(lines[2]));
-        }
-
-        private static V ParseButton(string input) {
-            var match = Regex.Match(input, @"X\+(\d+), Y\+(\d+)");
-            return new V(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-        }
-
-        private static V ParsePrize(string input) {
-            var match = Regex.Match(input, @"X=(\d+), Y=(\d+)");
-            return new V(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+            var numberRegex = new Regex(@"\d+");
+            var numbers = lines.Select(line => numberRegex.Matches(line).Select(x => x.Value.ToInt()).ToArray())
+                .Select(x => new V(x[0], x[1]))
+                .ToArray();
+            return new Machine(numbers[0], numbers[1], numbers[2]);
         }
     }
 
@@ -24,22 +18,29 @@ public static partial class Day13 {
             .Select(Machine.Parse)
             .ToArray();
 
-        machines.Select(Solve).Sum().Part1();
+        machines.Select(CountTokensToPrize).Sum().Part1();
 
         var part2Shift = new V(10000000000000, 10000000000000);
         machines.Select(x => x with { Prize = x.Prize + part2Shift })
-            .Select(Solve)
+            .Select(CountTokensToPrize)
             .Sum()
             .Part2();
     }
 
-    private static long Solve(Machine machine) {
-        var matrix = new Rational[][] {
-            [new(machine.ButtonA.X), new Rational(machine.ButtonB.X), new Rational(machine.Prize.X)],
-            [new(machine.ButtonA.Y), new Rational(machine.ButtonB.Y), new Rational(machine.Prize.Y)],
-        };
-        var solution = matrix.SolveGaussian();
-        if (solution.Any(x => x.Denominator != 1)) return 0;
-        return (long)(solution[0].Numerator * 3 + solution[1].Numerator);
+    private static long CountTokensToPrize(Machine machine) {
+        var (t1, t2) = SolveLinearEquation(machine.ButtonA, machine.ButtonB, machine.Prize);
+        return 3 * t1 + t2;
+    }
+
+    // ax*t1 + bx*t2 = px
+    // ay*t1 + by*t2 = py
+    private static V SolveLinearEquation(V a, V b, V p) {
+        var det = a.X * b.Y - a.Y * b.X;
+        if (det == 0) return V.Zero;
+        var (t1, rem1) = Math.DivRem(p.X * b.Y - p.Y * b.X, det);
+        if (rem1 != 0) return V.Zero;
+        var (t2, rem2) = Math.DivRem(a.X * p.Y - a.Y * p.X, det);
+        if (rem2 != 0) return V.Zero;
+        return new(t1, t2);
     }
 }
