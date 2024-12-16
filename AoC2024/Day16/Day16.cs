037fb204a8b1ab803b49367e40934ed4a1fd466d
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2024-12-16 11:56:47 +0400

    fix

--- AoC2024/Day16/Day16.cs
+++ AoC2024/Day16/Day16.cs
@@ -5,6 +5,7 @@ public static partial class Day16 {
         public State Forward() => this with { Position = Position + Direction };
         public State TurnCW() => this with { Direction = new V(Direction.Y, -Direction.X) };
         public State TurnCCW() => this with { Direction = new V(-Direction.Y, Direction.X) };
+        public State TurnAround() => this with { Direction = -Direction };
     }
 
     public static void Solve(IEnumerable<string> input) {
@@ -12,36 +13,31 @@ public static partial class Day16 {
         var start = map.FindFirst('S');
         var end = map.FindFirst('E');
         var initialState = new State(start, V.E);
+        var endStates = V.Directions4.Select(dir => new State(end, dir)).ToArray();
 
-        var bestDistance = SearchHelpers.Dijkstra(GetNextState, initialState)
-            .First(x => x.State.Position == end)
-            .Distance
+        var startToEnd = FindMinDistances([initialState]);
+        var bestDistance = endStates
+            .Min(x => startToEnd.GetValueOrDefault(x, int.MaxValue))
             .Part1();
 
-        var tiles = new HashSet<V>();
-        var queue = new PriorityQueue<SearchPathItem<State>, int>();
-        queue.Enqueue(SearchPathItem.Start(initialState), 0);
-        var visited = new Dictionary<State, int>();
-        while (queue.TryDequeue(out var current, out _)) {
-            if (current.State.Position == end) {
-                tiles.UnionWith(current.BackToStart().Select(x => x.Position));
-                continue;
-            }
+        var endToStart = FindMinDistances(endStates);
 
-            foreach (var next in GetNextState(current)) {
-                if (next.Distance <= bestDistance && visited.GetValueOrDefault(next.State, int.MaxValue) >= next.Distance) {
-                    visited[next.State] = next.Distance;
-                    queue.Enqueue(next, next.Distance);
-                }
-            }
-        }
+        startToEnd
+            .Where(x => x.Value + endToStart[x.Key.TurnAround()] == bestDistance)
+            .Select(x => x.Key.Position)
+            .Distinct()
+            .Count()
+            .Part2();
 
-        tiles.Count.Part2();
+        IReadOnlyDictionary<State, int> FindMinDistances(State[] initialStates) =>
+            SearchHelpers
+                .Dijkstra(GetNextState, initialStates)
+                .GroupBy(x => x.State, x => x.Distance)
+                .ToDictionary(x => x.Key, x => x.Min());
 
         IEnumerable<SearchPathItem<State>> GetNextState(SearchPathItem<State> current) {
             var (state, _, _) = current;
-            if (map[state.Position + state.Direction] != '#')
-                yield return current.Next(state.Forward());
+            if (map[state.Position + state.Direction] != '#') yield return current.Next(state.Forward());
 
             yield return current.Next(state.TurnCW(), 1000);
             yield return current.Next(state.TurnCCW(), 1000);
